21473f11ba456de53f73ef30001cf6a9aef6d82a
AdrianoRuberto <adriano.ruberto@heig-vd.ch>
2016-05-05 10:21:54 +0200

    Add arena

---
+++ src/ch/heigvd/bomberman/common/game/Arena.java
@@ -0,0 +1,15 @@
+package ch.heigvd.bomberman.common.game;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Projet : GEN_Projet
+ * Créé le 05.05.2016.
+ *
+ * @author Adriano Ruberto
+ */
+public class Arena {
+   List<Element> elements = new LinkedList<>();
+
+}
