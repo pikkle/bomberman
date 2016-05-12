5e909e51efc8abc92c9c44b7e5589a1ea5b5628b
matthieuVillard <matthieu.villard@heig-vd.ch>
2016-05-12 15:14:34 +0200

    create grid builder

---
+++ src/ch/heigvd/bomberman/common/game/Arena/GridBuilder.java
@@ -0,0 +1,16 @@
+package ch.heigvd.bomberman.common.game.Arena;
+
+/**
+ * Created by matthieu.villard on 12.05.2016.
+ */
+public class GridBuilder
+{
+	private Arena arena;
+
+	public GridBuilder(Arena arena){
+		this.arena = arena;
+	}
+
+	public
+
+}
