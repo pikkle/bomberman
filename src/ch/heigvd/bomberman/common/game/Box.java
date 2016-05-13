c5a6e307a18eac5b6da84fada65dea79bcaf4f11
AdrianoRuberto <adriano.ruberto@heig-vd.ch>
2016-05-13 09:48:08 +0200

    Rename bomber, delete useless observator pattern

--- src/ch/heigvd/bomberman/common/game/Box.java
+++ src/ch/heigvd/bomberman/common/game/Box.java
@@ -1,14 +1,11 @@
 package ch.heigvd.bomberman.common.game;
 
 import ch.heigvd.bomberman.common.game.Arena.Arena;
-import ch.heigvd.bomberman.common.game.bombs.Bomb;
 import ch.heigvd.bomberman.common.game.powerups.AddBombPowerUp;
 import ch.heigvd.bomberman.common.game.powerups.PowerUp;
 import javafx.geometry.Point2D;
 import javafx.scene.image.ImageView;
 
-import java.util.Observable;
-import java.util.Observer;
 import java.util.Optional;
 
 /**
@@ -17,18 +14,10 @@ import java.util.Optional;
  *
  * @author Adriano Ruberto
  */
-public class Box extends DestructibleElement implements Observer {
+public class Box extends DestructibleElement {
 
 	public Box(Point2D position, Arena arena) {
 		super(position, new ImageView(new javafx.scene.image.Image("ch/heigvd/bomberman/client/img/box.png")), arena);
-	}
-
-	@Override
-	public void update(Observable o, Object arg) {
-		if (o instanceof Bomb) { // Une bombe à explosé // TODO Notifier uniquement les observateur dans la range
-			arena.remove(this);
-			open().ifPresent(arena::add);
-		}
 	}
 
 	public Optional<PowerUp> open() {
