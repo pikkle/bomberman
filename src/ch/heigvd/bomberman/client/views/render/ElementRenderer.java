9a06adb32e1b6dc2ae70b1f2ea3a7a29eb76d0d6
AdrianoRuberto <adriano.ruberto@heig-vd.ch>
2016-06-02 22:25:35 +0200

    Add function to renderer

--- src/ch/heigvd/bomberman/client/views/render/ElementRenderer.java
+++ src/ch/heigvd/bomberman/client/views/render/ElementRenderer.java
@@ -15,101 +15,110 @@ import java.util.UUID;
 /**
  * Created by matthieu.villard on 18.05.2016.
  */
-public class ElementRenderer implements ElementVisitor
-{
+public class ElementRenderer implements ElementVisitor {
 	private HashMap<UUID, ImageViewPane> rendered = new HashMap<>();
 
 	@Override
 	public void visit(Wall wall) {
-		if(!rendered.containsKey(wall.getUuid()))
-			rendered.put(wall.getUuid(), new ImageViewPane(new ImageView(new Image("ch/heigvd/bomberman/client/img/wall.png"))));
+		if (!rendered.containsKey(wall.getUuid()))
+			rendered.put(wall.getUuid(), getImageViewPane("ch/heigvd/bomberman/client/img/wall.png"));
 	}
 
 	@Override
 	public void visit(Box box) {
-		if(!rendered.containsKey(box.getUuid()))
-			rendered.put(box.getUuid(), new ImageViewPane(new ImageView(new Image("ch/heigvd/bomberman/client/img/box.png"))));
+		if (!rendered.containsKey(box.getUuid()))
+			rendered.put(box.getUuid(), getImageViewPane("ch/heigvd/bomberman/client/img/box.png"));
 	}
 
 	@Override
 	public void visit(AddBombPowerUp powerUp) {
-		if(!rendered.containsKey(powerUp.getUuid()))
-			rendered.put(powerUp.getUuid(), new ImageViewPane(new ImageView(new Image("ch/heigvd/bomberman/client/img/powerups/addBomb.png"))));
+		if (!rendered.containsKey(powerUp.getUuid()))
+			rendered.put(powerUp.getUuid(), getImageViewPane("ch/heigvd/bomberman/client/img/powerups/addBomb.png"));
 	}
 
 	@Override
 	public void visit(AddBlastRangePowerUp powerUp) {
-		if(!rendered.containsKey(powerUp.getUuid()))
-			rendered.put(powerUp.getUuid(), new ImageViewPane(new ImageView(new Image("ch/heigvd/bomberman/client/img/powerups/blastRange.png"))));
+		if (!rendered.containsKey(powerUp.getUuid()))
+			rendered.put(powerUp.getUuid(), getImageViewPane("ch/heigvd/bomberman/client/img/powerups/blastRange.png"));
 	}
 
 	@Override
 	public void visit(Bomb bomb) {
-		if(!rendered.containsKey(bomb.getUuid()))
-			rendered.put(bomb.getUuid(), new ImageViewPane(new ImageView(new javafx.scene.image.Image("ch/heigvd/bomberman/client/img/bomb.png"))));
+		if (!rendered.containsKey(bomb.getUuid()))
+			rendered.put(bomb.getUuid(), getImageViewPane("ch/heigvd/bomberman/client/img/bomb.png"));
 	}
 
 	@Override
 	public void visit(Bomberman bomberman) {
-		if(!rendered.containsKey(bomberman.getUuid()))
-			rendered.put(bomberman.getUuid(), new ImageViewPane(new ImageView(new Image("ch/heigvd/bomberman/client/img/skins/" + bomberman.getSkin() + ".png"))));
+		if (!rendered.containsKey(bomberman.getUuid()))
+			rendered.put(bomberman.getUuid(), getImageViewPane("ch/heigvd/bomberman/client/img/skins/" + bomberman.getSkin() + ".png"));
 	}
 
 	@Override
 	public void visit(StartPoint startPoint) {
-		if(!rendered.containsKey(startPoint.getUuid())) {
+		if (!rendered.containsKey(startPoint.getUuid())) {
 			Iterator<StartPoint> iterator = startPoint.arena().getStartPoints().iterator();
 			int index;
-			for(index = 0; iterator.hasNext() && iterator.next() != startPoint; index++);
-			rendered.put(startPoint.getUuid(), new ImageViewPane(new ImageView(new javafx.scene.image.Image("ch/heigvd/bomberman/client/img/skins/" + Skin.values()[index % Skin.values().length] + ".png"))));
+			for (index = 0; iterator.hasNext() && iterator.next() != startPoint; index++) ;
+			rendered.put(startPoint.getUuid(), getImageViewPane("ch/heigvd/bomberman/client/img/skins/" + Skin.values()[index % Skin.values().length] + ".png"));
 		}
 	}
 
 	@Override
 	public void visit(HorizontalExplosion explosion) {
-		if(!rendered.containsKey(explosion.getUuid()))
-			rendered.put(explosion.getUuid(), new ImageViewPane(new ImageView(new javafx.scene.image.Image("ch/heigvd/bomberman/client/img/explosion/explosionH.png"))));
+		if (!rendered.containsKey(explosion.getUuid()))
+			rendered.put(explosion.getUuid(), getImageViewPane("ch/heigvd/bomberman/client/img/explosion/explosionH.png"));
 	}
 
 	@Override
 	public void visit(LeftExplosion explosion) {
-		if(!rendered.containsKey(explosion.getUuid()))
-			rendered.put(explosion.getUuid(), new ImageViewPane(new ImageView(new javafx.scene.image.Image("ch/heigvd/bomberman/client/img/explosion/explosionL.png"))));
+		if (!rendered.containsKey(explosion.getUuid()))
+			rendered.put(explosion.getUuid(), getImageViewPane("ch/heigvd/bomberman/client/img/explosion/explosionL.png"));
 	}
 
 	@Override
 	public void visit(RightExplosion explosion) {
-		if(!rendered.containsKey(explosion.getUuid()))
-			rendered.put(explosion.getUuid(), new ImageViewPane(new ImageView(new javafx.scene.image.Image("ch/heigvd/bomberman/client/img/explosion/explosionR.png"))));
+		if (!rendered.containsKey(explosion.getUuid()))
+			rendered.put(explosion.getUuid(), getImageViewPane("ch/heigvd/bomberman/client/img/explosion/explosionR.png"));
 	}
 
 	@Override
 	public void visit(VerticalExplosion explosion) {
-		if(!rendered.containsKey(explosion.getUuid()))
-			rendered.put(explosion.getUuid(), new ImageViewPane(new ImageView(new javafx.scene.image.Image("ch/heigvd/bomberman/client/img/explosion/explosionV.png"))));
+		if (!rendered.containsKey(explosion.getUuid()))
+			rendered.put(explosion.getUuid(), getImageViewPane("ch/heigvd/bomberman/client/img/explosion/explosionV.png"));
 	}
 
 	@Override
 	public void visit(TopExplosion explosion) {
-		if(!rendered.containsKey(explosion.getUuid()))
-			rendered.put(explosion.getUuid(), new ImageViewPane(new ImageView(new javafx.scene.image.Image("ch/heigvd/bomberman/client/img/explosion/explosionT.png"))));
+		if (!rendered.containsKey(explosion.getUuid()))
+			rendered.put(explosion.getUuid(), getImageViewPane("ch/heigvd/bomberman/client/img/explosion/explosionT.png"));
 	}
 
 	@Override
 	public void visit(BottomExplosion explosion) {
-		if(!rendered.containsKey(explosion.getUuid()))
-			rendered.put(explosion.getUuid(), new ImageViewPane(new ImageView(new javafx.scene.image.Image("ch/heigvd/bomberman/client/img/explosion/explosionB.png"))));
+		if (!rendered.containsKey(explosion.getUuid()))
+			rendered.put(explosion.getUuid(), getImageViewPane("ch/heigvd/bomberman/client/img/explosion/explosionB.png"));
 	}
 
 	@Override
 	public void visit(CentralExplosion explosion) {
-		if(!rendered.containsKey(explosion.getUuid()))
-			rendered.put(explosion.getUuid(), new ImageViewPane(new ImageView(new javafx.scene.image.Image("ch/heigvd/bomberman/client/img/explosion/explosionC.png"))));
+		if (!rendered.containsKey(explosion.getUuid()))
+			rendered.put(explosion.getUuid(), getImageViewPane("ch/heigvd/bomberman/client/img/explosion/explosionC.png"));
 	}
 
-	public ImageViewPane getSprite(Element element){
-		if(!rendered.containsKey(element.getUuid()))
+	public ImageViewPane getSprite(Element element) {
+		if (!rendered.containsKey(element.getUuid()))
 			return null;
 		return rendered.get(element.getUuid());
+	}
+
+	/**
+	 * Return a ImageViewPane from a path.
+	 *
+	 * @param path the path to the image
+	 * @return the ImageViewPane
+	 */
+	private ImageViewPane getImageViewPane(String path) {
+		return new ImageViewPane(new ImageView(new Image(path)));
 	}
 }
